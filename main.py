ea9b75623ca24f6b749b0ddf1575a21678079b47
Flint Mueller <flintthecuber@gmail.com>
2025-06-02 10:52:47 -0400

    class and 2 swings

--- main.py
+++ main.py
@@ -1,58 +1,66 @@
 from math import cos, sin
 from vpython import sign, sphere, vector, color, rate, graph, gcurve
-
-ang_velocity_graph = graph(
-    title="Pendulum Angular Velocity",
-    xtitle="Time (s)",
-    ytitle="Velocity (m/s)",
-    scroll=True,
-    xmin=0,
-    xmax=1000,
-)
-pendulum_graph = gcurve(color=color.red, graph=ang_velocity_graph)
-phase_space_graph = graph(
-    title="Pendulum Phase Space",
-    xtitle="Angle (rad)",
-    ytitle="Angular Velocity (rad/s)",
-)
-pendulum_phase_space = gcurve(color=color.blue, graph=phase_space_graph)
-
-pendulum_length = 20
-pendulum = sphere(pos=vector(0, -pendulum_length, 0), radius=1, color=color.red)
-pendulum_mass = 15
-pendulum_ang_velocity = 0
-pendulum_ang = 0.2
-pendulum_moment = pendulum_mass * pendulum_length**2 / 2
-gravity = vector(0, -9.81, 0)
-
-friction_coefficient = 0.3
-
-external_force_amplitude = 393
-external_force_frequency = 0.51111
-
-push_angle = 0.15
-effective_angle_diff = 0.1
-
+    
 t = 0
 dt = 0.1
 
+class Swing:
+    def __init__(self, v_color, ps_color, length, mass, ang_velo, ang, friction_coefficient, external_force_amplitude, external_force_frequency, push_angle, effective_angle_diff):
+        self.ang_velocity_graph = graph(
+            title="Pendulum Angular Velocity",
+            xtitle="Time (s)",
+            ytitle="Velocity (m/s)",
+            scroll=True,
+            xmin=0,
+            xmax=1000,
+        )
+        self.pendulum_graph = gcurve(color=v_color, graph=self.ang_velocity_graph)
+        self.phase_space_graph = graph(
+            title="Pendulum Phase Space",
+            xtitle="Angle (rad)",
+            ytitle="Angular Velocity (rad/s)",
+        )
+        self.pendulum_phase_space = gcurve(color=ps_color, graph=self.phase_space_graph)
+        
+        self.pendulum_length = length
+        self.pendulum = sphere(pos=vector(0, -self.pendulum_length, 0), radius=1, color=color.red)
+        self.pendulum_mass = mass
+        self.pendulum_ang_velocity = ang_velo
+        self.pendulum_ang = ang
+        self.pendulum_moment = self.pendulum_mass * self.pendulum_length**2 / 2
+        self.gravity = vector(0, -9.81, 0)
+        
+        self.friction_coefficient = friction_coefficient
+        
+        self.external_force_amplitude = external_force_amplitude
+        self.external_force_frequency = external_force_frequency
+        
+        self.push_angle = push_angle
+        self.effective_angle_diff = effective_angle_diff
+
+    def update(self):
+        self.friction_force = -self.friction_coefficient * self.pendulum_ang_velocity
+        external_force = self.external_force_amplitude * cos(self.external_force_frequency * t) if abs(self.pendulum_ang) < self.push_angle else 0
+        torque = (
+            (self.pendulum_mass * self.gravity).cross(self.pendulum.pos)
+            + self.friction_force * self.pendulum_length * vector(0, 0, -1)
+            + external_force * abs(cos(self.pendulum_ang + self.effective_angle_diff)) * vector(0, 0, -1)
+        )
+        ang_accel = -(torque.mag * sign(torque.z)) / self.pendulum_moment
+        self.pendulum_ang_velocity += ang_accel * dt
+        self.pendulum_ang += self.pendulum_ang_velocity * dt
+        self.pendulum.pos = vector(
+            self.pendulum_length * sin(self.pendulum_ang), -self.pendulum_length * cos(self.pendulum_ang), 0
+        )
+    
+        self.pendulum_graph.plot(t, self.pendulum_ang_velocity)
+        self.pendulum_phase_space.plot(self.pendulum_ang, self.pendulum_ang_velocity)
+
+swing1 = Swing(color.red, color.blue, 20, 15, 0, 0.2, 0.3, 393, 0.51111, 0.15, 0.1)
+swing2 = Swing(color.green, color.orange, 15, 15, 0, 0.2, 0.3, 393, 0.51111, 0.15, 0.1)
+    
 while True:
     rate(1000)
-    friction_force = -friction_coefficient * pendulum_ang_velocity
-    external_force = external_force_amplitude * cos(external_force_frequency * t) if abs(pendulum_ang) < push_angle else 0
-    torque = (
-        (pendulum_mass * gravity).cross(pendulum.pos)
-        + friction_force * pendulum_length * vector(0, 0, -1)
-        + external_force * abs(cos(pendulum_ang + effective_angle_diff)) * vector(0, 0, -1)
-    )
-    ang_accel = -(torque.mag * sign(torque.z)) / pendulum_moment
-    pendulum_ang_velocity += ang_accel * dt
-    pendulum_ang += pendulum_ang_velocity * dt
-    pendulum.pos = vector(
-        pendulum_length * sin(pendulum_ang), -pendulum_length * cos(pendulum_ang), 0
-    )
-
-    pendulum_graph.plot(t, pendulum_ang_velocity)
-    pendulum_phase_space.plot(pendulum_ang, pendulum_ang_velocity)
-
+    swing1.update()
+    swing2.update()
     t += dt
