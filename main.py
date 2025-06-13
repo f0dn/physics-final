0bb3337450d44a378571e83117066eebba9b82b6
Daniel Park <dpark070111@gmail.com>
2025-06-13 09:42:50 -0400

    sliders almost

--- main.py
+++ main.py
@@ -10,7 +10,66 @@ images = {
     "MOM_FLINT": ["https://i.ibb.co/V0cyPxV6/mom-flint.jpg", 0.1],
 }
 
-length = 0
+length = 10
+mass = 5
+ang_velo = 0
+ang = 0.2
+friction_coefficient = 0.3
+external_force_amplitude = 393
+external_force_frequency = 0.51111
+push_angle = 0.15
+effective_angle_diff = 0.1
+
+def update_length(slider):
+    global length
+    length = slider.value
+
+def update_mass(slider):
+    global mass
+    mass = slider.value
+
+def update_ang_velo(slider):
+    global ang_velo
+    ang_velo = slider.value
+
+def update_ang(slider):
+    global ang
+    ang = slider.value
+
+def update_friction(slider):
+    global friction_coefficient
+    friction_coefficient = slider.value
+
+def update_external_force_amplitude(slider):
+    global external_force_amplitude
+    external_force_amplitude = slider.value
+
+def update_external_force_frequency(slider):
+    global external_force_frequency
+    external_force_frequency = slider.value
+
+def update_push_angle(slider):
+    global push_angle
+    push_angle = slider.value
+
+def update_effective_angle_diff(slider):
+    global effective_angle_diff
+    effective_angle_diff = slider.value
+
+# control_scene = canvas(title='Parameters', width=400, height=600, range=1)
+# slider_x = 250
+# slider_y_start = 550
+# slider_spacing = 40
+
+# slider_length = slider(scene=control_scene, min=1, max=50, value=length, length=150, bind=update_length, pos=vector(slider_x, slider_y_start, 0), label='Length')
+# slider_mass = slider(scene=control_scene, min=1, max=20, value=mass, length=150, bind=update_mass, pos=vector(slider_x, slider_y_start - slider_spacing, 0), label='Mass')
+# slider_ang_velo = slider(scene=control_scene, min=-2, max=2, value=ang_velo, length=150, bind=update_ang_velo, pos=vector(slider_x, slider_y_start - 2*slider_spacing, 0), label='Angular Vel')
+# slider_ang = slider(scene=control_scene, min=0, max=3.14, value=ang, length=150, bind=update_ang, pos=vector(slider_x, slider_y_start - 3*slider_spacing, 0), label='Angle')
+# slider_friction = slider(scene=control_scene, min=0, max=1, value=friction_coefficient, length=150, bind=update_friction, pos=vector(slider_x, slider_y_start - 4*slider_spacing, 0), label='Friction Coef')
+# slider_external_force_amp = slider(scene=control_scene, min=0, max=1000, value=external_force_amplitude, length=150, bind=update_external_force_amplitude, pos=vector(slider_x, slider_y_start - 5*slider_spacing, 0), label='External Force Amp')
+# slider_external_force_freq = slider(scene=control_scene, min=0, max=10, value=external_force_frequency, length=150, bind=update_external_force_frequency, pos=vector(slider_x, slider_y_start - 6*slider_spacing, 0), label='External Force Freq')
+# slider_push_angle = slider(scene=control_scene, min=0, max=1, value=push_angle, length=150, bind=update_push_angle, pos=vector(slider_x, slider_y_start - 7*slider_spacing, 0), label='Push Angle')
+# slider_effective_angle_diff = slider(scene=control_scene, min=0, max=1, value=effective_angle_diff, length=150, bind=update_effective_angle_diff, pos=vector(slider_x, slider_y_start - 8*slider_spacing, 0), label='Eff. Angle Diff')
 
 class Swing:
     def __init__(
@@ -93,24 +152,18 @@ class Swing:
         self.pendulum_graph.plot(t, self.pendulum_ang_velocity)
         self.pendulum_phase_space.plot(self.pendulum_ang, self.pendulum_ang_velocity)
 
-
-def set_length(evt):
-    length = evt.value
-
-length_slider = slider(bind=set_length, min=0, max=50)
-
 swing1 = Swing(
     color.red,
     color.blue,
-    20,
-    15,
-    0,
-    0.2,
-    0.3,
-    393,
-    0.51111,
-    0.15,
-    0.1,
+    length,
+    mass,
+    ang_velo,
+    ang,
+    friction_coefficient,
+    external_force_amplitude,
+    external_force_frequency,
+    push_angle,
+    effective_angle_diff,
     "MOM_FLINT",
     "BABY_DANIEL",
 )
@@ -134,7 +187,6 @@ swing2 = Swing(
 scene.visible = False
 scene.waitfor("textures")
 scene.visible = True
-
 
 while True:
     rate(60)
